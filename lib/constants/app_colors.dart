f947b4dcc8b8576e62b658b4d1c4d5b4b82c89a9
musseGkel <musseg437@gmail.com>
2024-06-19 23:47:01 +0200

    constant files

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF000000);
+  static const Color accentColor = Color(0xFF34A853);
+  static const Color backgroundColor = Color(0xFFF4F4F4);
+  static const Color errorColor = Color(0xFFD32F2F);
+  static const Color successColor = Color(0xFF4CAF50);
+  static const Color buttonPrimaryColor = Color(0xFF2196F3);
+  static const Color buttonSecondaryColor = Color(0xFFFFC107);
+  static const Color primaryTextColor = Color(0xFFFFFFFF);
+  static const Color secondaryTextColor = Color(0xFF000000);
+
+  static const Color primaryBorderColor = Color(0xFFFFFFFF);
+  static const Color borderSecondaryColor = Color(0xFF707070);
+  static const Color greyColor = Color(0xFF707070);
+  static const Color borderColor = Color(0xFF4E4E4E);
+  static const Color paleGrayColor = Color(0xFFF1F3F5);
+}
