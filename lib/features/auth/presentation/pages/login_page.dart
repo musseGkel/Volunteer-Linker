dc904f6df86d850090a8d1d6063b175150443c9c
musseGkel <musseg437@gmail.com>
2024-06-22 15:26:13 +0200

    Add common text field widget for login page

--- lib/features/auth/presentation/pages/login_page.dart
+++ lib/features/auth/presentation/pages/login_page.dart
@@ -26,108 +26,89 @@ class _LoginScreenState extends State<LoginScreen> {
     return BlocBuilder<AuthBloc, AuthState>(
       builder: (context, state) {
         return Scaffold(
-            backgroundColor: Colors.grey[200],
-            body: Form(
-              key: _formKey,
-              child: Stack(children: [
-                Align(
-                  alignment: Alignment.center,
-                  child: SizedBox(
-                    width: 500,
-                    height: size.height,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Container(
-                        width: size.width * 0.85,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 20, vertical: 30),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: const [
-                            BoxShadow(
-                              color: Colors.black12,
-                              blurRadius: 20,
-                              offset: Offset(0, 10),
-                            )
-                          ],
-                        ),
-                        child: SingleChildScrollView(
-                          child: Center(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: <Widget>[
-                                Center(
-                                  child: Text(
-                                    state.isLogin ? "Login" : "Register",
-                                    style: const TextStyle(
-                                      fontSize: 30,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                SizedBox(height: size.height * 0.06),
-                                CommonTextField(
-                                  label: "EMAIL",
-                                  controller: _controllerEmail,
-                                ),
-                                SizedBox(height: size.height * 0.03),
-                                CommonTextField(
-                                  label: "Password",
-                                  controller: _controllerPassword,
-                                ),
-                                if (state.errorMessage != '')
-                                  Column(
-                                    children: [
-                                      SizedBox(height: size.height * 0.04),
-                                      ErrorMessage(
-                                        errorMessage: state.errorMessage,
-                                      ),
-                                    ],
-                                  ),
-                                if (state.isLoading)
-                                  Column(
-                                    children: [
-                                      SizedBox(height: size.height * 0.04),
-                                      const LoadingIndicator(),
-                                    ],
-                                  ),
-                                SizedBox(height: size.height * 0.04),
-                                SubmitButton(
-                                  isLogin: state.isLogin,
-                                  controllerEmail: _controllerEmail,
-                                  controllerPassword: _controllerPassword,
-                                ),
-                                SizedBox(height: size.height * 0.04),
-                                TextButton(
-                                  onPressed: () {
-                                    BlocProvider.of<AuthBloc>(context).add(
-                                      ToggleLoginEvent(state),
-                                    );
-                                  },
-                                  child: Text(
-                                    state.isLogin
-                                        ? "Register Instead"
-                                        : "Login Instead",
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      color: Colors.black.withOpacity(0.7),
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
+          backgroundColor: Colors.grey[200],
+          body: Form(
+            key: _formKey,
+            child: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Center(
+                    child: Text(
+                      state.isLogin ? "Login" : "Register",
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ),
-              ]),
-            ));
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 35,
+                      left: 40,
+                      right: 40,
+                    ),
+                    child: CommonTextField(
+                      label: "EMAIL",
+                      controller: _controllerEmail,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      top: 35,
+                      left: 40,
+                      right: 40,
+                    ),
+                    child: CommonTextField(
+                      label: "Password",
+                      controller: _controllerPassword,
+                    ),
+                  ),
+                  if (state.errorMessage != '')
+                    Column(
+                      children: [
+                        SizedBox(height: size.height * 0.04),
+                        ErrorMessage(
+                          errorMessage: state.errorMessage,
+                        ),
+                      ],
+                    ),
+                  if (state.isLoading)
+                    Column(
+                      children: [
+                        SizedBox(height: size.height * 0.04),
+                        const LoadingIndicator(),
+                      ],
+                    ),
+                  SizedBox(height: size.height * 0.04),
+                  SubmitButton(
+                    isLogin: state.isLogin,
+                    controllerEmail: _controllerEmail,
+                    controllerPassword: _controllerPassword,
+                  ),
+                  SizedBox(height: size.height * 0.04),
+                  TextButton(
+                    onPressed: () {
+                      BlocProvider.of<AuthBloc>(context).add(
+                        ToggleLoginEvent(state),
+                      );
+                    },
+                    child: Text(
+                      state.isLogin ? "Register Instead" : "Login Instead",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(0.7),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
       },
     );
   }
