b07c36fa11ea17df4c4f726c5375d7f85d932a32
musseGkel <musseg437@gmail.com>
2024-06-24 02:23:29 +0200

    Refactor auth_datasource.dart for improved user registration process

--- lib/features/auth/data/datasources/auth_datasource.dart
+++ lib/features/auth/data/datasources/auth_datasource.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../../../../core/enums.dart';
+import '../../../../core/models/organization.dart';
+import '../../../../core/models/user.dart';
 
 class AuthDatasource {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
@@ -36,33 +38,36 @@ class AuthDatasource {
     User? user = userCredential.user;
     if (user != null) {
       if (userType == UserType.volunteer) {
-        await _firestore.collection('users').doc(user.uid).set({
-          'id': user.uid,
-          'name': name.trim(),
-          'email': user.email,
-          'userType': enumToString(userType),
-          'profilePictureUrl': '',
-          'interests': [],
-          'skills': [],
-          'availability': '',
-          'volunteerActivities': [],
-        });
+        await _firestore.collection('users').doc(user.uid).set(
+              UserData(
+                id: user.uid,
+                name: name.trim(),
+                email: user.email!,
+                userType: userType,
+                profilePictureUrl: '',
+                interests: [],
+                skills: [],
+                availability: '',
+                volunteerActivities: [],
+              ).toJson(),
+            );
       } else if (userType == UserType.organization) {
         await _firestore.collection("organization").doc(user.uid).set(
-          {
-            'id': user.uid,
-            'name': name.trim(),
-            'email': user.email,
-            'userType': enumToString(userType),
-            'contactNumber': '',
-            'description': '',
-            'address': '',
-            'profilePictureUrl': '',
-            'postedOpportunites': [],
-          },
-        );
+              Organization(
+                id: user.uid,
+                name: name.trim(),
+                email: user.email!,
+                userType: userType,
+                contactNumber: '',
+                description: '',
+                address: '',
+                profilePictureUrl: '',
+                postedOpportunities: [],
+              ).toJson(),
+            );
       }
     }
+
     return currentUser;
   }
 
