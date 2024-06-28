16830fea3e3a658f42359f089d7fda45daaad69b
musseGkel <musseg437@gmail.com>
2024-06-28 18:27:38 +0200

    refactor: Add color constant to AppColors

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  static const Color transparent = Color(0x00000000);
   static const Color primaryColor = Color(0xFF000000);
   static const Color accentColor = Color(0xFF34A853);
   static const Color backgroundColor = Color(0xFFF4F4F4);
@@ -16,4 +17,5 @@ class AppColors {
   static const Color greyColor = Color(0xFF707070);
   static const Color borderColor = Color(0xFF4E4E4E);
   static const Color paleGrayColor = Color(0xFFF1F3F5);
+  static const Color softGray = Color(0xFFCCCCCC);
 }
