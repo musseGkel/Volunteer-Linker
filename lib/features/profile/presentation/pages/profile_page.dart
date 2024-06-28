1cb9c0d9afbb39dc6630da888ac38fa2f48ce9cd
musseGkel <musseg437@gmail.com>
2024-06-28 18:29:40 +0200

    feat: Add profile page with basic layout and styling

---
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:volunteer_linker/constants/app_colors.dart';
+import '../widgets/profile_item.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.transparent,
+        elevation: 0,
+        leading: const Icon(Icons.arrow_back, color: AppColors.backgroundColor),
+        actions: const [
+          Icon(
+            Icons.settings,
+            color: AppColors.backgroundColor,
+          ),
+          SizedBox(width: 16),
+        ],
+      ),
+      extendBodyBehindAppBar: true,
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 50,
+            ),
+            height: 350,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [AppColors.softGray, AppColors.secondaryTextColor],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40),
+                bottomRight: Radius.circular(40),
+              ),
+            ),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(
+                      "https://via.placeholder.com/150"), // Replace with actual image URL
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "James Martin",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  false ? "Volunteer" : "Organization",
+                  style: TextStyle(
+                    color: AppColors.primaryTextColor,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "1000",
+                          style: TextStyle(
+                            color: AppColors.primaryTextColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Followers",
+                          style: TextStyle(
+                            color: AppColors.primaryTextColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(width: 40),
+                    Column(
+                      children: [
+                        Text(
+                          "1200",
+                          style: TextStyle(
+                            color: AppColors.primaryTextColor,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Following",
+                          style: TextStyle(
+                            color: AppColors.primaryTextColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(16),
+              children: [
+                ProfileItem(
+                  title: 'Name',
+                  subtitle:
+                      'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet',
+                  onTap: () {},
+                ),
+                ProfileItem(
+                  title: 'Username',
+                  subtitle: '',
+                  onTap: () {},
+                ),
+                ProfileItem(
+                  title: 'Posted Opportunities',
+                  subtitle: '',
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
