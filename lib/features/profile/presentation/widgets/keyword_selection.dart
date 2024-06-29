38fc0d7d03777c7075346352a6f33225b2c27eee
musseGkel <musseg437@gmail.com>
2024-06-29 16:04:32 +0200

    refactor: Update KeywordsSelection widget to use Flutter Bloc for state management

--- lib/features/profile/presentation/widgets/keyword_selection.dart
+++ lib/features/profile/presentation/widgets/keyword_selection.dart
@@ -1,92 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:volunteer_linker/constants/app_colors.dart';
+import 'package:volunteer_linker/features/profile/presentation/bloc/bloc/keyword_selection_bloc.dart';
 
-class KeywordsSelection extends StatefulWidget {
+class KeywordsSelection extends StatelessWidget {
   final List<String> keywords;
   final String title;
   final bool canEdit;
-
+  final Function(List<String> selectedKeywords)? onSave;
   const KeywordsSelection({
     super.key,
     required this.keywords,
     required this.title,
     this.canEdit = false,
+    this.onSave,
   });
 
   @override
-  _KeywordsSelectionState createState() => _KeywordsSelectionState();
-}
-
-class _KeywordsSelectionState extends State<KeywordsSelection> {
-  List<String> selectedKeywords = [];
-  bool isEditMode = false;
-
-  void _toggleKeyword(String keyword) {
-    setState(() {
-      if (selectedKeywords.contains(keyword)) {
-        selectedKeywords.remove(keyword);
-      } else {
-        selectedKeywords.add(keyword);
-      }
-    });
-  }
-
-  void _toggleEditMode() {
-    setState(() {
-      isEditMode = !isEditMode;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        if (widget.canEdit)
-          Align(
-            alignment: Alignment.topRight,
-            child: IconButton(
-              icon: Icon(isEditMode ? Icons.check : Icons.edit),
-              onPressed: _toggleEditMode,
-            ),
-          ),
-        if (isEditMode && widget.canEdit)
-          Wrap(
-            spacing: 8.0,
-            runSpacing: 4.0,
-            children: widget.keywords.map((keyword) {
-              final bool isSelected = selectedKeywords.contains(keyword);
-              return FilterChip(
-                label: Text(keyword),
-                selected: isSelected,
-                onSelected: (bool selected) {
-                  _toggleKeyword(keyword);
-                },
-                selectedColor: AppColors.greyColor,
-                backgroundColor: AppColors.backgroundColor,
-                checkmarkColor: AppColors.primaryIconColor,
-              );
-            }).toList(),
-          ),
-        Text(
-          widget.title,
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        Wrap(
-          spacing: 8.0,
-          runSpacing: 4.0,
-          children: selectedKeywords.map((keyword) {
-            return Chip(
-              label: Text(keyword),
-              deleteIcon: isEditMode ? const Icon(Icons.cancel) : null,
-              onDeleted: isEditMode
-                  ? () {
-                      _toggleKeyword(keyword);
+    return BlocBuilder<KeywordSelectionBloc, KeywordSelectionState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            if (canEdit)
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  icon: Icon(state.isEditMode ? Icons.check : Icons.edit),
+                  onPressed: () {
+                    if (state.isEditMode) {
+                      onSave!(state.selectedKeywords);
                     }
-                  : null,
-            );
-          }).toList(),
-        ),
-      ],
+
+                    BlocProvider.of<KeywordSelectionBloc>(context).add(
+                      ToggleEditMode(
+                        state: state,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            if (state.isEditMode && canEdit)
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 4.0,
+                children: keywords.map((keyword) {
+                  return FilterChip(
+                    label: Text(keyword),
+                    selected: state.isSelected(keyword),
+                    onSelected: (bool selected) {
+                      BlocProvider.of<KeywordSelectionBloc>(context).add(
+                        ToggleKeyword(
+                          state: state,
+                          keyword: keyword,
+                        ),
+                      );
+                    },
+                    selectedColor: AppColors.greyColor,
+                    backgroundColor: AppColors.backgroundColor,
+                    checkmarkColor: AppColors.primaryIconColor,
+                  );
+                }).toList(),
+              ),
+            Text(
+              title,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Wrap(
+              spacing: 8.0,
+              runSpacing: 4.0,
+              children: state.selectedKeywords.map(
+                (keyword) {
+                  return Chip(
+                    label: Text(keyword),
+                    deleteIcon:
+                        state.isEditMode ? const Icon(Icons.cancel) : null,
+                    onDeleted: state.isEditMode
+                        ? () {
+                            BlocProvider.of<KeywordSelectionBloc>(context).add(
+                              ToggleKeyword(
+                                state: state,
+                                keyword: keyword,
+                              ),
+                            );
+                          }
+                        : null,
+                  );
+                },
+              ).toList(),
+            ),
+          ],
+        );
+      },
     );
   }
 }
