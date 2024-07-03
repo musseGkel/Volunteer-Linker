6c8b465f7326f0451ff3ec5922c33baa20eb21cc
musseGkel <musseg437@gmail.com>
2024-07-04 01:05:42 +0200

    refactor: Update ProfileDetail widgets

--- lib/features/profile/presentation/widgets/profile_detail.dart
+++ lib/features/profile/presentation/widgets/profile_detail.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:volunteer_linker/constants/app_colors.dart';
-
-import 'EditProfileDetailDialog.dart';
+import 'package:volunteer_linker/core/enums.dart';
+import 'package:volunteer_linker/features/profile/presentation/widgets/edit_profile_detail_dialog.dart';
 
 class ProfileDetail extends StatelessWidget {
   final String title;
@@ -9,6 +9,7 @@ class ProfileDetail extends StatelessWidget {
   final Function(String)? onEditPressed;
   final bool isOnEditMode;
   final Function()? onTap;
+  final ProfileDetailEditType editType;
 
   const ProfileDetail({
     super.key,
@@ -17,13 +18,14 @@ class ProfileDetail extends StatelessWidget {
     this.isOnEditMode = true,
     this.onEditPressed,
     this.onTap,
+    required this.editType,
   });
 
   void _showEditDialog(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
-        return EditProfileDetail(
+        return EditProfileDetailDialog(
           initialValue: subtitle,
           title: title,
           onSave: (newSubtitle) {
@@ -38,74 +40,78 @@ class ProfileDetail extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 4.0),
-        child: Material(
-          elevation: 15,
-          borderRadius: BorderRadius.circular(10),
-          child: Stack(
-            children: [
-              Container(
-                padding: EdgeInsets.only(
-                  top: 6,
-                  bottom: 6,
-                  left: isOnEditMode ? 10 : 10,
-                  right: isOnEditMode ? 25 : 10,
-                ),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: const Border(
-                    bottom: BorderSide(
-                      color: AppColors.greyColor,
-                      width: 0.5,
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            child: Material(
+              elevation: 15,
+              borderRadius: BorderRadius.circular(10),
+              child: Stack(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 6,
+                      bottom: 6,
+                      left: isOnEditMode ? 10 : 10,
+                      right: isOnEditMode ? 25 : 10,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: const Border(
+                        bottom: BorderSide(
+                          color: AppColors.greyColor,
+                          width: 0.5,
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      textBaseline: TextBaseline.alphabetic,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            title,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              subtitle,
+                              maxLines: 3,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ),
-                child: Row(
-                  textBaseline: TextBaseline.alphabetic,
-                  crossAxisAlignment: CrossAxisAlignment.baseline,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        title,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                  if (isOnEditMode)
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: IconButton(
+                        onPressed: () => _showEditDialog(context),
+                        icon: const Icon(
+                          Icons.edit,
+                          color: AppColors.primaryColor,
                         ),
                       ),
                     ),
-                    Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          subtitle,
-                          maxLines: 3,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                ],
               ),
-              if (isOnEditMode)
-                Positioned(
-                  top: 0,
-                  right: 0,
-                  child: IconButton(
-                    onPressed: () => _showEditDialog(context),
-                    icon: const Icon(
-                      Icons.edit,
-                      color: AppColors.primaryColor,
-                    ),
-                  ),
-                ),
-            ],
+            ),
           ),
         ),
-      ),
+      ],
     );
   }
 }
---
+++ lib/features/profile/presentation/widgets/profile_detail_expansion_tile.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:volunteer_linker/core/enums.dart';
+import 'package:volunteer_linker/features/profile/presentation/widgets/keyword_selection.dart';
+
+import '../bloc/profile_detail_bloc/profile_detail_bloc.dart';
+import 'availability_widget.dart';
+
+class ProfileDetailExpansionTile extends StatelessWidget {
+  final ProfileDetailType profileDetailType;
+  final bool editMode;
+  const ProfileDetailExpansionTile({
+    super.key,
+    required this.profileDetailType,
+    required this.editMode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProfileDetailBloc, ProfileDetailState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0),
+          child: Material(
+            elevation: 5,
+            borderRadius: BorderRadius.circular(10),
+            child: ExpansionTile(
+              title: Text(
+                state.title(profileDetailType),
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              children: [
+                if (profileDetailType == ProfileDetailType.skills ||
+                    profileDetailType == ProfileDetailType.interests)
+                  Padding(
+                    padding: const EdgeInsets.all(
+                      8.0,
+                    ),
+                    child: KeywordsSelection(
+                      keywords: state.keywords(profileDetailType),
+                      title: state.title(profileDetailType),
+                      canEdit: editMode,
+                      onSave: (value) {},
+                    ),
+                  ),
+                if (profileDetailType == ProfileDetailType.volunteerActivities)
+                  Padding(
+                    padding: const EdgeInsets.all(
+                      8.0,
+                    ),
+                    child: Column(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Charity",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Spacer(),
+                              Text(
+                                "Hours of contribution",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ...Map.from(state.volunteerActivities())
+                            .entries
+                            .map((entry) {
+                          final String charity = entry.key;
+                          final int hours = entry.value;
+
+                          // return SizedBox();
+                          return ListTile(
+                            title: Text(
+                              "$charity : ",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            trailing: SizedBox(
+                              width: 50,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    hours.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        }),
+                      ],
+                    ),
+                  ),
+                if (profileDetailType == ProfileDetailType.availability)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AvailabilityWidget(),
+                  ),
+                const SizedBox(height: 10),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
