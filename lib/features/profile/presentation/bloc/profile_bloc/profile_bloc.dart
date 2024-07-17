d0eb33e9c9462c4b09cc7d2e1a34f5de1b11e63c
musseGkel <musseg437@gmail.com>
2024-07-17 23:46:55 +0200

    refactor: Update ProfileBloc to get use id on intialization

--- lib/features/profile/presentation/bloc/profile_bloc/profile_bloc.dart
+++ lib/features/profile/presentation/bloc/profile_bloc/profile_bloc.dart
@@ -1,13 +1,32 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:volunteer_linker/core/models/user.dart';
+import 'package:volunteer_linker/features/profile/data/datasource/profile_datasource.dart';
+import 'package:volunteer_linker/features/profile/data/repository/profile_repository_impl.dart';
+import 'package:volunteer_linker/services/api_reponse.dart';
+
+import '../../../../../core/enums.dart';
 
 part 'profile_event.dart';
 part 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  ProfileBloc() : super(ProfileInitial()) {
-    on<ProfileEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  ProfileBloc()
+      : super(
+          ProfileState(userId: FirebaseAuth.instance.currentUser?.uid ?? ''),
+        ) {
+    on<ProfileEvent>(
+      (event, emit) async {
+        await emit.forEach(
+          event.handle(),
+          onData: (state) {
+            return state;
+          },
+        );
+      },
+    );
   }
 }
