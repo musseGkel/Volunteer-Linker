05c8d75269c0aef8bc1c1aeaea915527708c1735
musseGkel <musseg437@gmail.com>
2024-07-17 23:43:44 +0200

    refactor: Update routing page to use ProfilePage instead of HomePage

--- lib/features/auth/presentation/pages/routing_page.dart
+++ lib/features/auth/presentation/pages/routing_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../profile/presentation/bloc/profile_bloc/profile_bloc.dart';
+import '../../../profile/presentation/pages/profile_page.dart';
 import '../bloc/auth_bloc.dart';
-import 'home_page.dart';
 import 'login_page.dart';
 
 class RoutingPage extends StatelessWidget {
@@ -10,12 +11,19 @@ class RoutingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ProfileBloc>(
+          create: (context) => ProfileBloc(),
+        ),
+        BlocProvider<AuthBloc>(
+          create: (context) => AuthBloc(),
+        ),
+      ],
       child: BlocBuilder<AuthBloc, AuthState>(
         builder: (context, state) {
           if (state.user != null) {
-            return const HomePage();
+            return const ProfilePage();
           } else {
             return const LoginScreen();
           }
