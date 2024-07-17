95882b11f275b5a13a92c5b6b49c570fbe3d9ca6
musseGkel <musseg437@gmail.com>
2024-07-17 23:43:21 +0200

    refactor: Add username and bio fields to UserData model

--- lib/core/models/user.dart
+++ lib/core/models/user.dart
@@ -3,47 +3,53 @@ import '../enums.dart';
 class UserData {
   final String id;
   final String name;
+  final String username;
   final String email;
+  final String bio;
   final UserType userType;
   final String profilePictureUrl;
-  final List<String> interests;
-  final List<String> skills;
-  final String availability;
-  final List<String> volunteerActivities;
+  final List<dynamic> interests;
+  final List<dynamic> skills;
+  final List<dynamic> availability;
+  final List<dynamic> volunteerActivities;
 
   UserData({
     required this.id,
     required this.name,
     required this.email,
+    required this.username,
+    this.bio = "",
     this.userType = UserType.volunteer,
     this.profilePictureUrl = "",
     this.interests = const [],
     this.skills = const [],
-    this.availability = "",
+    this.availability = const [],
     this.volunteerActivities = const [],
   });
 
-  // Convert a User objecx`t to a map
   Map<String, dynamic> toJson() {
     return {
       'id': id,
       'name': name,
       'email': email,
+      'username': username,
       'userType': enumToString(userType),
       'profilePictureUrl': profilePictureUrl,
       'interests': interests,
       'skills': skills,
       'availability': availability,
       'volunteerActivities': volunteerActivities,
+      'bio': bio,
     };
   }
 
-  // Create a User object from a map
   factory UserData.fromJson(Map<String, dynamic> json) {
     return UserData(
       id: json['id'],
       name: json['name'],
       email: json['email'],
+      username: json['username'],
+      bio: json['bio'],
       userType: enumFromString(UserType.values, json['userType']),
       profilePictureUrl: json['profilePictureUrl'],
       interests: List<String>.from(json['interests']),
