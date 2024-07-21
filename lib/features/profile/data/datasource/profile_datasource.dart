8064830b42cfb93278a2bd83ae95938cee478198
musseGkel <musseg437@gmail.com>
2024-07-21 22:58:02 +0200

    refactor: Add organization profile functionality to ProfileDatasource

--- lib/features/profile/data/datasource/profile_datasource.dart
+++ lib/features/profile/data/datasource/profile_datasource.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:volunteer_linker/core/models/organization.dart';
+import 'package:volunteer_linker/core/models/profile_info.dart';
 import 'package:volunteer_linker/core/models/user_data.dart';
 import '../../../../services/api_reponse.dart';
 
@@ -26,6 +28,25 @@ class ProfileDatasource {
     }
   }
 
+  Future<ApiResponse> getOrganizationProfile(userId) async {
+    try {
+      var response =
+          await _firestore.collection('organizations').doc(userId).get();
+      return ApiResponse(
+        statusCode: 200,
+        message: "Success",
+        body: Organization.fromJson(
+          response.data()!,
+        ),
+      );
+    } catch (e) {
+      return ApiResponse(
+        statusCode: 400,
+        message: "Error",
+      );
+    }
+  }
+
   Future<ApiResponse> updateProfile({
     required UserData user,
   }) async {
@@ -36,6 +57,46 @@ class ProfileDatasource {
       return ApiResponse(
         statusCode: 200,
         message: "Success",
+      );
+    } catch (e) {
+      return ApiResponse(
+        statusCode: 400,
+        message: "Error",
+      );
+    }
+  }
+
+  Future<ApiResponse> updateOrganizationProfile({
+    required Organization organization,
+  }) async {
+    try {
+      await _firestore.collection('organizations').doc(organization.id).set(
+            organization.toJson(),
+          );
+      return ApiResponse(
+        statusCode: 200,
+        message: "Success",
+      );
+    } catch (e) {
+      return ApiResponse(
+        statusCode: 400,
+        message: "Error",
+      );
+    }
+  }
+
+  Future<ApiResponse> getProfileInfo(
+    userId,
+  ) async {
+    try {
+      var response =
+          await _firestore.collection('profileInfo').doc(userId).get();
+      return ApiResponse(
+        statusCode: 200,
+        message: "Success",
+        body: ProfileInfo.fromJson(
+          response.data() ?? {},
+        ),
       );
     } catch (e) {
       return ApiResponse(
