57769dbb8faf4a2b507b1ce30aca35866a32dbc0
musseGkel <musseg437@gmail.com>
2024-07-29 15:47:41 +0200

    chore: Add support for loading environment variables from .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'features/auth/presentation/pages/routing_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // Check if the .env file exists
+  if (File(".env").existsSync()) {
+    await dotenv.load(fileName: ".env"); // Load environment variables
+  } else {
+    print("Error: .env file not found");
+  }
+
   runApp(const MyApp());
 }
 
