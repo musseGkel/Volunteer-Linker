5360c3c5c96c910e3a2a109f1c47724d616d7e4b
musseGkel <musseg437@gmail.com>
2024-08-01 09:30:35 +0200

    feat: Add OpportunityDatasource for posting opportunities

---
+++ lib/features/opportunity/data/datasource/opportunity_datasource.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../../../services/api_reponse.dart';
+import '../model/opportunity.dart';
+
+class OpportunityDatasource {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  User? get currentUser => _firebaseAuth.currentUser;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future postOpportunity({required Opportunity opportunity}) async {
+    try {
+      if (currentUser == null) {
+        return ApiResponse(
+          statusCode: 400,
+          message: "Error",
+        );
+      } else {
+        var response =
+            await _firestore.collection('posts').doc(opportunity.id).set(
+                  opportunity.toJson(),
+                );
+
+        return ApiResponse(
+          statusCode: 200,
+          message: "Success",
+        );
+      }
+    } catch (e) {
+      return ApiResponse(
+        statusCode: 400,
+        message: "Error",
+      );
+    }
+  }
+}
