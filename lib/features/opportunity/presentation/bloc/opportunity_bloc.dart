d94d8eb803b182d8d4faca16e25d7f613d2bf98d
musseGkel <musseg437@gmail.com>
2024-08-01 09:30:29 +0200

    refactor: Update OpportunityBloc to include necessary dependencies and imports

--- lib/features/opportunity/presentation/bloc/opportunity_bloc.dart
+++ lib/features/opportunity/presentation/bloc/opportunity_bloc.dart
@@ -1,9 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uuid/uuid.dart';
 import 'package:volunteer_linker/features/opportunity/data/model/location_model.dart';
 
 import '../../../../constants/keyword_constants.dart';
+import '../../../../services/api_reponse.dart';
+import '../../data/datasource/opportunity_datasource.dart';
+import '../../data/model/opportunity.dart';
+import '../../data/repository/opportunity_repository_impl.dart';
+import '../../domain/usecases/post_opportunity_usecase.dart';
 
 part 'opportunity_event.dart';
 part 'opportunity_state.dart';
