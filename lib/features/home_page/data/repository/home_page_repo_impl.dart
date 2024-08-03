ae84563e214165f208f8e72b7662626c06b9ce32
musseGkel <musseg437@gmail.com>
2024-08-03 23:24:07 +0200

    refactor: Implement HomePageRepoImpl for fetching posts

---
+++ lib/features/home_page/data/repository/home_page_repo_impl.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:volunteer_linker/features/home_page/domain/repository/home_page_repository.dart';
+import 'package:volunteer_linker/services/api_reponse.dart';
+
+import '../datasource/home_page_datasource.dart';
+
+class HomePageRepoImpl implements HomePageRepository {
+  final HomePageDatasource dataSource;
+
+  HomePageRepoImpl(
+    this.dataSource,
+  );
+
+  @override
+  Future<ApiResponse> fetchPosts(
+      {DocumentSnapshot<Object?>? startAfter, required int limit}) {
+    return dataSource.fetchPosts(
+      startAfter: startAfter,
+      limit: limit,
+    );
+  }
+}
