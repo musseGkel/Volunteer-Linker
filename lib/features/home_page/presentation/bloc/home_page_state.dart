bc4472c6c002816b62202bf15dedc1d67f93795d
musseGkel <musseg437@gmail.com>
2024-08-03 23:29:02 +0200

    refactor: Update HomePageState to include PostStatus and necessary fields

--- lib/features/home_page/presentation/bloc/home_page_state.dart
+++ lib/features/home_page/presentation/bloc/home_page_state.dart
@@ -1,10 +1,41 @@
 part of 'home_page_bloc.dart';
 
+enum PostStatus { initial, success, failure }
+
 class HomePageState extends Equatable {
-  const HomePageState();
+  final PostStatus status;
+  final List<Opportunity> opportunities;
+  final bool hasReachedMax;
+  final DocumentSnapshot? lastDocument;
+
+  const HomePageState({
+    this.status = PostStatus.initial,
+    this.opportunities = const [],
+    this.hasReachedMax = false,
+    this.lastDocument,
+  });
+
+  HomePageState copyWith({
+    PostStatus? status,
+    List<Opportunity>? opportunities,
+    bool? hasReachedMax,
+    DocumentSnapshot? lastDocument,
+  }) {
+    return HomePageState(
+      status: status ?? this.status,
+      opportunities: opportunities ?? this.opportunities,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      lastDocument: lastDocument ?? this.lastDocument,
+    );
+  }
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [
+        status,
+        opportunities,
+        hasReachedMax,
+        lastDocument,
+      ];
 
   String getTimeAgo(
     DateTime dateTime,
