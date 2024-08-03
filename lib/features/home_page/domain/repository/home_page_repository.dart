8e98a376ecbce694edc7701bf1b63bea45175a7a
musseGkel <musseg437@gmail.com>
2024-08-03 23:23:59 +0200

    feat: Add HomePageRepository interface for fetching posts

---
+++ lib/features/home_page/domain/repository/home_page_repository.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../../../services/api_reponse.dart';
+
+abstract class HomePageRepository {
+  HomePageRepository();
+
+  Future<ApiResponse> fetchPosts({
+    DocumentSnapshot<Object?>? startAfter,
+    required int limit,
+  });
+}
