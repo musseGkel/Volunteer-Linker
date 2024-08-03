c17cb17172ef0ed343f73f52c0551829193783ad
musseGkel <musseg437@gmail.com>
2024-08-03 23:28:50 +0200

    feat: Implement lazy loading for fetching posts

--- lib/features/home_page/presentation/bloc/home_page_event.dart
+++ lib/features/home_page/presentation/bloc/home_page_event.dart
@@ -8,3 +8,71 @@ sealed class HomePageEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class FetchPostsEvent extends HomePageEvent {
+  final HomePageState state;
+
+  const FetchPostsEvent({
+    required this.state,
+  });
+
+  @override
+  List<Object> get props => [state];
+
+  @override
+  Stream<HomePageState> handle() async* {
+    ApiResponse response;
+
+    if (state.hasReachedMax) return;
+
+    try {
+      if (state.status == PostStatus.initial) {
+        response = await FetchPostsUseCase(
+          HomePageRepoImpl(
+            HomePageDatasource(),
+          ),
+        ).call(
+          startAfter: null,
+          limit: 10,
+        );
+
+        var data = response.body as Map<String, dynamic>;
+        List<Opportunity> opportunities = data['opportunities'];
+        DocumentSnapshot? lastDocument = data['lastDocument'];
+
+        yield state.copyWith(
+          status: PostStatus.success,
+          opportunities: opportunities,
+          lastDocument: lastDocument,
+          hasReachedMax: response.body.length < 10,
+        );
+      } else if (state.status == PostStatus.success) {
+        response = await FetchPostsUseCase(
+          HomePageRepoImpl(
+            HomePageDatasource(),
+          ),
+        ).call(
+          startAfter: state.lastDocument,
+          limit: 10,
+        );
+        var data = response.body as Map<String, dynamic>;
+        List<Opportunity> opportunities = data['opportunities'];
+        DocumentSnapshot? lastDocument = data['lastDocument'];
+
+        yield response.body.isEmpty
+            ? state.copyWith(hasReachedMax: true)
+            : state.copyWith(
+                status: PostStatus.success,
+                opportunities: List.of(state.opportunities)
+                  ..addAll(opportunities),
+                lastDocument: lastDocument,
+                hasReachedMax: response.body.length < 10,
+              );
+      }
+    } catch (_) {
+      yield state.copyWith(
+        status: PostStatus.failure,
+      );
+    }
+  }
+}
