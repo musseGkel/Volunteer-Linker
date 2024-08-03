7ddec480b690068b0848de539deb781b96b6f4a1
musseGkel <musseg437@gmail.com>
2024-08-03 23:29:34 +0200

    feat: implement HomePageBloc

--- lib/features/home_page/presentation/bloc/home_page_bloc.dart
+++ lib/features/home_page/presentation/bloc/home_page_bloc.dart
@@ -1,6 +1,13 @@
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:intl/intl.dart';
+import 'package:volunteer_linker/services/api_reponse.dart';
+
+import '../../../opportunity/data/model/opportunity.dart';
+import '../../data/datasource/home_page_datasource.dart';
+import '../../data/repository/home_page_repo_impl.dart';
+import '../../domain/usecases/fetch_posts_usecase.dart';
 
 part 'home_page_event.dart';
 part 'home_page_state.dart';
